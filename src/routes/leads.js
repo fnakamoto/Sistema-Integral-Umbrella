df72cd22bec4d7b1edbef459a9405a36733ae580
fnakamoto <fabiano@nakamoto.adv.br>
2025-06-27 21:39:53 -0300

    Update leads.js

--- src/routes/leads.js
+++ src/routes/leads.js
@@ -1,6 +1,12 @@
-/src
-  /routes
-    leads.js
-  /controllers
-    leadsController.js
-  app.js (ou index.js)
+const express = require('express');
+const router = express.Router();
+const leadsController = require('../controllers/leadsController');
+
+router.post('/', leadsController.criarLead);
+router.get('/', leadsController.listarLeads);
+router.get('/estatisticas', leadsController.estatisticas);
+router.put('/:id', leadsController.atualizarLead);
+router.patch('/:id/etapa', leadsController.alterarEtapa);
+router.delete('/:id', leadsController.deletarLead);
+
+module.exports = router;
