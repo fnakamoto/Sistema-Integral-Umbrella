c3f1d2923a3181671aa26e44d4ce10e47ae2f954
fnakamoto <fabiano@nakamoto.adv.br>
2025-06-27 21:44:33 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const leadsRouter = require('./routes/leads');
 const financeiroRouter = require('./routes/financeiro');
+const peticoesRouter = require('./routes/peticoes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,9 +10,10 @@ app.use(express.json());
 
 app.use('/api/leads', leadsRouter);
 app.use('/api/financeiro', financeiroRouter);
+app.use('/api/peticoes', peticoesRouter);
 
 app.get('/', (req, res) => {
-  res.send('API Umbrella - Gestão de Leads e Financeiro funcionando');
+  res.send('API Umbrella - Gestão de Leads, Financeiro e Peticionamento funcionando');
 });
 
 app.listen(PORT, () => {
