0de4dd8b37b9141b9d86ea9b226cf507a76b9277
fnakamoto <fabiano@nakamoto.adv.br>
2025-06-28 09:09:00 -0300

    Create permissoes.js

---
+++ backend/config/permissoes.js
@@ -0,0 +1,6 @@
+module.exports = {
+  Master: ["ver_todos_leads", "editar_etapas", "gerar_boletos", "acessar_financeiro", "administrar_usuarios"],
+  Consultor: ["ver_leads", "editar_etapas"],
+  Financeiro: ["ver_financeiro", "gerar_boletos"],
+  Atendimento: ["ver_leads", "editar_atendimento"]
+};
